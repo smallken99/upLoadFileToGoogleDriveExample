b48955a28b7e62f9944e1af2757d950ab2a72846
smallken99 <smallken@gmail.com>
2020-01-22 16:14:46 +0800

    first commit

---
+++ start.py
@@ -0,0 +1,23 @@
+'''
+There are (at least) a couple of libraries openly available for this now:
+* pystray
+* infi.systray
+I just started using infi.systray in a project, and it's worked well for me. Here's how little code you need to do something very basic (taken from their docs):
+from infi.systray import SysTrayIcon
+def say_hello(systray):
+    print("Hello, World!")
+menu_options = (("Say Hello", None, say_hello),)
+systray = SysTrayIcon("icon.ico", "Example tray icon", menu_options)
+systray.start()
+
+
+'''
+from infi.systray import SysTrayIcon
+import uploadMyMoneydb as up
+def say_hello(systray):
+    print("Hello, World!")
+menu_options = (("Say Hello", None, say_hello),
+		("檔案上傳",None,up.main)
+	)
+systray = SysTrayIcon("icon/scriptx.ico", "Example tray icon", menu_options)
+systray.start()
